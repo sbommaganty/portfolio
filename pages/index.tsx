5f031056c5ba23567242be3e08704096079902dc
Ariful Alam <alamariful1727@gmail.com>
2023-01-05 16:10:22 -0500

    fix: common layout style fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <SEO />
-      <div>
+      <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
         <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
           Ariful Alam
         </h1>
