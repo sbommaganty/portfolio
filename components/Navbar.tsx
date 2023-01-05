1a152d27622921549b9dddf859433fca609b826c
Ariful Alam <alamariful1727@gmail.com>
2023-01-05 15:55:25 -0500

    feat: Mobile menu onClick handled

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,6 +7,8 @@ import { outerNavigation, innerNavigation, contactNavigation } from "config";
 const Navbar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+  const handleMobileMenuClose = () => setMobileMenuOpen(false);
+
   return (
     <div className="px-6 pt-6 lg:px-8">
       <nav
@@ -68,7 +70,11 @@ const Navbar = () => {
         <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white p-6 lg:hidden">
           <div className="flex h-9 items-center justify-between">
             <div className="flex">
-              <Link href="/" className="-m-1.5 p-1.5">
+              <Link
+                href="/"
+                className="-m-1.5 p-1.5"
+                onClick={handleMobileMenuClose}
+              >
                 <span className="sr-only">Title</span>
                 <svg
                   className="h-8"
@@ -100,7 +106,7 @@ const Navbar = () => {
               <button
                 type="button"
                 className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
+                onClick={handleMobileMenuClose}
               >
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon className="h-6 w-6" aria-hidden="true" />
@@ -115,6 +121,7 @@ const Navbar = () => {
                     key={name}
                     href={href}
                     className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                    onClick={handleMobileMenuClose}
                   >
                     {name}
                   </Link>
@@ -124,6 +131,7 @@ const Navbar = () => {
                 <Link
                   href={contactNavigation.href}
                   className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                  onClick={handleMobileMenuClose}
                 >
                   {contactNavigation.name}
                 </Link>
