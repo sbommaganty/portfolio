ded0e32d61d7c4f826416a0fb1cc8de6751d448f
Ariful Alam <alamariful1727@gmail.com>
2023-01-16 14:32:42 -0500

    feat: footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -65,7 +65,7 @@ const social = [
 
 export default function Footer() {
   return (
-    <footer className="mx-auto max-w-7xl overflow-hidden py-12 px-6 lg:px-8">
+    <footer className="mx-auto max-w-7xl space-y-4 overflow-hidden p-6 sm:space-y-5">
       <nav
         className="-mx-5 -my-2 flex flex-wrap justify-center"
         aria-label="Footer"
@@ -74,28 +74,28 @@ export default function Footer() {
           <div key={item.name} className="px-5 py-2">
             <Link
               href={item.href}
-              className="text-base text-gray-500 transition hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+              className="text-base text-gray-600 transition hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
             >
               {item.name}
             </Link>
           </div>
         ))}
       </nav>
-      <div className="mt-8 flex justify-center space-x-6">
+      <div className="flex justify-center space-x-6">
         {social.map((item) => (
           <a
             key={item.name}
             href={item.href}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-gray-500 transition hover:text-gray-500 dark:text-gray-400 dark:hover:text-white"
+            className="text-gray-500 transition hover:text-gray-700 dark:text-gray-400 dark:hover:text-white"
           >
             <span className="sr-only">{item.name}</span>
             <item.icon className="h-6 w-6" aria-hidden={true} />
           </a>
         ))}
       </div>
-      <p className="mt-8 text-center text-base text-gray-400 dark:text-gray-500">
+      <p className="text-center text-base text-gray-500 dark:text-gray-500">
         &copy; {new Date().getFullYear()} Ariful Alam. All rights reserved.
       </p>
     </footer>
