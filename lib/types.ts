f2f4cbaa322675f2352e5d5249fa0a77e55b8837
Ariful Alam <alamariful1727@gmail.com>
2023-02-01 22:57:03 -0500

    feat: Experience type added

--- lib/types.ts
+++ lib/types.ts
@@ -9,3 +9,24 @@ export type ProjectData = {
   isProfessional: boolean;
   image: StaticImageData;
 };
+
+export type ExperienceData = {
+  company: {
+    name: string;
+    href: string;
+  };
+  position: string;
+  location: string;
+  duration: {
+    from: string;
+    to: string;
+  };
+  isWorkingNow: boolean;
+  image: StaticImageData;
+  description: string[];
+  technologies: {
+    name: string;
+    href: string;
+    color: string;
+  }[];
+};
