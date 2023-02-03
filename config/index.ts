a088ba6cbe92f64ff05bde0832f09accc5b446d8
Ariful Alam <alamariful1727@gmail.com>
2023-02-02 23:08:10 -0500

    feat: technologies data added in config

--- config/index.ts
+++ config/index.ts
@@ -32,3 +32,91 @@ export const navigation = {
 export const AboutPage = {
   skills: ["Node.js", "React.js", "Python", "Next.js", "AWS", "TypeScript"],
 };
+
+export const technologies = {
+  node: { name: "Node.js", href: "https://nodejs.org/" },
+  react: {
+    name: "React.js",
+    href: "https://reactjs.org/",
+  },
+  d3: { name: "D3", href: "https://d3js.org/" },
+  jest: { name: "Jest", href: "https://jestjs.io/" },
+  cypress: {
+    name: "Cypress",
+    href: "https://www.cypress.io/",
+  },
+  swagger: {
+    name: "Swagger",
+    href: "https://swagger.io/",
+  },
+  postgresql: {
+    name: "PostgreSQL",
+    href: "https://www.postgresql.org/",
+  },
+  mongodb: {
+    name: "MongoDB",
+    href: "https://www.mongodb.com/",
+  },
+  dynamodb: {
+    name: "DynamoDB",
+    href: "https://aws.amazon.com/dynamodb/",
+  },
+  aws: { name: "AWS", href: "https://aws.amazon.com/" },
+  python: {
+    name: "Python",
+    href: "https://www.python.org/",
+  },
+  echarts: {
+    name: "Apache ECharts",
+    href: "https://echarts.apache.org/",
+  },
+  appsync: {
+    name: "AppSync",
+    href: "https://aws.amazon.com/appsync/",
+  },
+  elementalMediaConvert: {
+    name: "Elemental MediaConvert",
+    href: "https://aws.amazon.com/mediaconvert/",
+  },
+  githubActions: {
+    name: "GitHub Actions",
+    href: "https://github.com/features/actions",
+  },
+  codePipeline: {
+    name: "CodePipeline",
+    href: "https://aws.amazon.com/codepipeline/",
+  },
+  codeBuild: {
+    name: "CodeBuild",
+    href: "https://aws.amazon.com/codebuild/",
+  },
+  next: { name: "Next.js", href: "https://nextjs.org/" },
+  ec2: {
+    name: "EC2",
+    href: "https://aws.amazon.com/ec2/",
+  },
+  lambda: {
+    name: "Lambda",
+    href: "https://aws.amazon.com/lambda/",
+  },
+  s3: {
+    name: "S3",
+    href: "https://aws.amazon.com/s3/",
+  },
+  cloudFront: {
+    name: "CloudFront",
+    href: "https://aws.amazon.com/cloudfront/",
+  },
+  pusher: {
+    name: "PusherJs",
+    href: "https://pusher.com/channels",
+  },
+  mocha: {
+    name: "Mocha",
+    href: "https://mochajs.org/",
+  },
+  chai: {
+    name: "Chai",
+    href: "https://www.chaijs.com/",
+  },
+};
